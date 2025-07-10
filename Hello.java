43e153636fcf9a98ce0a441471043f5179f2dcbf
Karan Naik <karannaik7725@gmail.com>
2025-07-10 19:12:21 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+package cosmos;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello world of finnbroh");
+		// TODO Auto-generated method stub
+
+	}
+
+}
